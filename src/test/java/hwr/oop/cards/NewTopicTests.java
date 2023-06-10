c3a1799267c889e2b7301afcf24829dbe4b6e582
dimitrod <dimtrod@googlemail.com>
2023-06-10 13:02:37 +0200

    Topic better mutation coverage

--- src/test/java/hwr/oop/cards/NewTopicTests.java
+++ src/test/java/hwr/oop/cards/NewTopicTests.java
@@ -3,6 +3,8 @@ package hwr.oop.cards;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class NewTopicTests {
@@ -18,25 +20,39 @@ class NewTopicTests {
     @Test
     void equalTopicSameObject(){
         Topic topic1 = new Topic("Spanisch");
-        assertThat(topic1).isEqualTo(topic1);
+        assertThat(topic1.equals(topic1)).isTrue();
     }
     @Test
     void equalTopics(){
         Topic topic1 = new Topic("Spanisch");
         Topic topic2 = new Topic("Spanisch");
-        assertThat(topic1).isEqualTo(topic2);
+        assertThat(topic1.equals(topic2)).isTrue();
     }
+
     @Test
     void notEqualTopics(){
         Topic topic1 = new Topic("Spanisch");
         Topic topic2 = new Topic("Portugisisch");
-        assertThat(topic1).isNotEqualTo(topic2);
+        assertThat(topic1.equals(topic2)).isFalse();
     }
+
     @Test
-    void notEqualTopicWithDifferentObject(){
+    void notEqualTopicsWithDifferentCardLists() {
         Topic topic1 = new Topic("Spanisch");
-        Card card = new Card("Karte?", "Ja", 1);
-        assertThat(topic1).isNotEqualTo(card);
+        topic1.createCard("Question 1", "Answer 1");
+
+        Topic topic2 = new Topic("Spanisch");
+        topic2.createCard("Question 2", "Answer 2");
+
+        assertThat(topic1.equals(topic2)).isFalse();
+    }
+
+    @Test
+    void notEqualTopicAndCard() {
+        Topic topic1 = new Topic("Topic 1");
+        Card topic2 = new Card(" Bob", "der Baumeeister", 1);
+
+        assertThat(topic1.equals(topic2)).isFalse();
     }
 
 }
